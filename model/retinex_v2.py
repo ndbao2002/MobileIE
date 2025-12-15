61ec57929e56cc3abc7ed998ff76ac518d839777
ndbao2002 <ndbao2002@outlook.com>
2025-12-15 13:24:43 +0000

    update retinex v2.1

--- model/retinex_v2.py
+++ model/retinex_v2.py
@@ -18,18 +18,19 @@ class MobileRetinexV2ISPNet(nn.Module):
         super(MobileRetinexV2ISPNet, self).__init__()
         self.channels = channels
 
-        # Head (Large receptive field)
-        self.head = MBRConv5(in_channels, channels, rep_scale=rep_scale)
+        # This gives us Receptive Field (Conv5) AND Quadratic Curve (FST) immediately.
+        self.head_conv = MBRConv5(in_channels, channels, rep_scale=rep_scale)
+        self.head_fst = RetinexFST(channels, rep_scale=rep_scale)
         
         # Body (Retinex Enhanced)
         self.body1 = nn.Sequential(
             MBRConv3(channels, channels, rep_scale=rep_scale),
-            # RetinexFST(channels, rep_scale=rep_scale),
+            RetinexFST(channels, rep_scale=rep_scale),
             RetinexHDPA(channels, rep_scale=rep_scale)
         )
         self.body2 = nn.Sequential(
             MBRConv3(channels, channels, rep_scale=rep_scale),
-            # RetinexFST(channels, rep_scale=rep_scale),
+            RetinexFST(channels, rep_scale=rep_scale),
             RetinexHDPA(channels, rep_scale=rep_scale)
         )
         
@@ -38,20 +39,27 @@ class MobileRetinexV2ISPNet(nn.Module):
             MBRConv1(channels, channels, rep_scale=rep_scale),
             nn.PixelShuffle(2),
             MBRConv1(channels // 4, out_channels, rep_scale=rep_scale),
-            nn.Sigmoid()  # Ensure output is in [0, 1] range
+            nn.Sigmoid() 
         )
         self.tail_warm = MBRConv3(channels, 4, rep_scale=rep_scale)
         self.drop = DropBlock(3)
 
     def forward(self, x):
-        x_shallow = self.head(x)
-        x1 = self.body1(x_shallow) + x_shallow
-        x2 = self.body2(x1) + x1
+        x_feat = self.head_conv(x)
+        x_shallow = self.head_fst(x_feat)
+
+        x1 = self.body1(x_shallow)
+        x1 = x1 + x_shallow
+        
+        x2 = self.body2(x1)
+        x2 = x2 + x1
+        
         return self.tail(x2)
 
     def forward_warm(self, x):
         x = self.drop(x)
-        x = self.head(x)
+        x = self.head_conv(x)
+        x = self.head_fst(x)
         x = self.body1(x)
         x = self.body2(x)
         return self.tail(x), self.tail_warm(x)
@@ -65,7 +73,7 @@ class MobileRetinexV2ISPNet(nn.Module):
                     w, b = mod.slim()
                     weight_slim['%s.weight' % name] = w 
                     weight_slim['%s.bias' % name] = b 
-            elif isinstance(mod, RetinexHDPA):
+            elif isinstance(mod, RetinexFST):
                 weight_slim['%s.bias' % name] = mod.bias
             elif isinstance(mod, nn.PReLU):
                 weight_slim['%s.weight' % name] = mod.weight
@@ -75,31 +83,37 @@ class MobileRetinexV2ISPNet(nn.Module):
 class MobileRetinexV2ISPNetS(nn.Module):
     def __init__(self, in_channels=4, out_channels=3, channels=32):
         super(MobileRetinexV2ISPNetS, self).__init__()
-        # Head (Large receptive field)
-        self.head = nn.Conv2d(in_channels, channels, 5, 1, 2)
         
-        # Body (Retinex Enhanced)
+        # Head: Conv5 + RetinexFST
+        self.head_conv = nn.Conv2d(in_channels, channels, 5, 1, 2)
+        self.head_fst = RetinexFSTS(channels)
+        
         self.body1 = nn.Sequential(
             nn.Conv2d(channels, channels, 3, 1, 1),
-            # RetinexFSTS(channels),
+            RetinexFSTS(channels),
             RetinexHDPAS(channels)
         )
         self.body2 = nn.Sequential(
             nn.Conv2d(channels, channels, 3, 1, 1),
-            # RetinexFSTS(channels),
+            RetinexFSTS(channels),
             RetinexHDPAS(channels)
         )
         
-        # Tail
         self.tail = nn.Sequential(
             nn.Conv2d(channels, channels, 1),
             nn.PixelShuffle(2),
             nn.Conv2d(channels // 4, out_channels, 1),
-            nn.Sigmoid()  # Ensure output is in [0, 1] range
+            nn.Sigmoid()
         )
  
     def forward(self, x):
-        x_shallow = self.head(x)
-        x1 = self.body1(x_shallow) + x_shallow
-        x2 = self.body2(x1) + x1
+        x_feat = self.head_conv(x)
+        x_shallow = self.head_fst(x_feat)
+        
+        x1 = self.body1(x_shallow)
+        x1 = x1 + x_shallow
+        
+        x2 = self.body2(x1)
+        x2 = x2 + x1
+        
         return self.tail(x2)
